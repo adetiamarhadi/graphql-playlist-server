695af95407c8ab42ac9928d9533a84f1a2118472
Adetia Marhadi <adetia.marhadi@prismalink.co.id>
2021-04-07 09:41:55 +0700

    connect to mongodb cloud (mlab)

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const URI = 'mongodb+srv://<user>:<password>@<host>/<dbName>?retryWrites=true&w=majority';
+mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
